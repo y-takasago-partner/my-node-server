2b7a3e986e46f8f7dd9a8c63554b006cd0815f84
y-takasago-partner <y-takasago@go-partner.jp>
2026-08-18 16:40:33 +0900

    new EndPoint add

--- Node-backend_8.js
+++ Node-backend_8.js
@@ -278,10 +278,76 @@ app.post('/kintone-webhook/', async (req, res) => {
     }
 });
 
-//app.get('/thanks.html', (req, res) => {
-//    // 同じ階層にある「thanks.html」を見つけてブラウザに送る
-//    res.sendFile(path.join(__dirname, 'thanks.html'));
-//});
+// **** kintone Webhook受信エンドポイント
+app.post('/kintone-SoudanUketsuke-webhook/', async (req, res) => {
+    console.log('--- kintone-SoudanUketsuke-webhookを受信しました ---');
+    const body = req.body;
+    //console.log(webhookData);
+    //console.log(webhookData.レコード番号);
+    //console.log(webhookData.氏名);
+    //console.log(webhookData.メールアドレス);
+
+    // kintoneの通知タイプが「レコード追加」であるかチェック
+    if (body.type !== 'ADD_RECORD') {
+        console.log(`スキップ: ${body.type} のイベントは処理しません。`);
+        return res.status(200).send('Record added event only.');
+    }
+    // kintoneのレコードデータを取得
+    const record = body.record;
+    const toEmail = record['電子メールアドレス'] ? record['電子メールアドレス'].value : null;
+    if (!toEmail) {
+        console.error('エラー: レコードにメールアドレスが含まれていません。');
+        return res.status(400).send('Email field is missing.');
+    }
+
+console.log('電子メールアドレス is ' + record['電子メールアドレス'].value);
+console.log('レコード番号 is ' + record['レコード番号'].value);
+
+return res.status(200).send('Webhook received successfully');
+
+
+
+
+    const WebSoudan_honbun = 
+        "(このメールは送信専用メールからお送りさせていただいております。ご返信いただいてもお答えできませんのでご注意ください。)\n\n" + 
+        "日本貸金業協会貸金業相談・紛争解決センターです。ご相談を受付けました。\n" + 
+        "受付日から３営業日以内に担当者からご連絡(050-3494-7988から発信)を差し上げますのでお待ちください。\n" + 
+        "なお、このメールにお心当たりがない場合は、お手数ですが貸金業相談・紛争解決センターまでご連絡ください。\n\n\n" + 
+        "※上記受付日とは、協会の通信機器がメールを受信した日とします。\n" + 
+        "※営業日とは、土曜日、日曜日、祝休日、年末年始休業日を除いた日をいいます。\n\n\n\n" + 
+        "【例】\n" + 
+        "受付日が月曜日の場合は、３営業日目の木曜日までにご連絡します。\n" + 
+        "※受付日が金曜日だった場合は、土曜日、日曜日を除き、翌週の水曜日が３営業日目となります。\n\n\n\n" + 
+        "【ご連絡先】\n" + 
+        "日本貸金業協会\n" + 
+        "貸金業相談・紛争解決センター\n" + 
+        "〒108-0074東京都港区高輪3-19-15 二葉高輪ビル2階\n" + 
+        "TEL 03-5739-3861/050-3494-7988 FAX 03-5739-3024\n";
+    const msg = {
+        to  :  record['電子メールアドレス'].value,          // 宛先メールアドレス
+        from: {
+          name : '日本貸金業協会　貸金業相談・紛争解決センター', // Fromの日本語表記
+          email: 'noreplywebjfsa@j-fsa.jp',      //From（SendGridで認証済みドメインのメールアドレス）
+        },
+        subject: sbjPreFix + 'ご相談受付けの件', // 件名
+        text: WebSoudan_honbun,                  // 本文
+    };
+    const url2 = 'URLをクリックしてください\n' + webSoudanUrl + webSoudanAppId + '/show#record=' + record['レコード番号'].value;
+    const msg2 = {
+        to  :  addrToSoudanStaff,                // 宛先メールアドレス
+        from:  'soudan@j-fsa.jp',                //From（SendGridで認証済みドメインのメールアドレス）
+        subject: sbjPreFix + '相談受付の件',     // 件名
+        text: url2 + '\n',                       // 本文
+    };
+    try {
+        res.status(200).send('Webhook received successfully');
+        sendEMail(msg);
+        sendEMail(msg2);
+    } catch (error) {
+        console.error('Webhook処理エラー:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
