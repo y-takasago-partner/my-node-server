f245f25c5c1b5ba9168a50a5e53be2d3e3a0a949
y-takasago-partner <y-takasago@go-partner.jp>
2026-08-21 05:53:10 +0900

    test

--- Node-backend_8.js
+++ Node-backend_8.js
@@ -36,7 +36,8 @@ async function sendEMail(msg) {
 }
 
 
-app.post('/kintone-webhook-dev', async (req, res) => {
+app.post('/kintone-webhook/', async (req, res) => {
+//app.post('/kintone-webhook-dev', async (req, res) => {
     console.log('--- kintone-webhook-dev を受信しました ---');
     const webhookData = req.body;
     const WebSoudan_honbun = 
