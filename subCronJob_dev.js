9f9484da572dfc1ca753bfdea2d865893c202ef7
y-takasago-partner <y-takasago@go-partner.jp>
2026-08-27 02:48:29 +0900

    test

--- subCronJob_dev.js
+++ subCronJob_dev.js
@@ -37,7 +37,7 @@ app.use(express.static('public'));                          // PDFファイル�
 // 定期実行（Cronタスク）の処理
 // ==========================================
 //const subCronJob_dev = (scheduleTime = '0 19 * * *') => {
-const subCronJob_dev = (scheduleTime = '46 02 * * *') => {
+const subCronJob_dev = (scheduleTime = '50 02 * * *') => {
   cron.schedule(scheduleTime, async () => {
     console.log('定期実行タスクを開始します...');
     // 実際の非同期処理をここに記述
@@ -60,6 +60,7 @@ const subCronJob_dev = (scheduleTime = '46 02 * * *') => {
             '  ) ' + 
             ' limit 10';
 console.log(query);
+return;
         const response = await kintoneClient.record.getRecords({
             app: JishukuSendAppID,
             query: query
